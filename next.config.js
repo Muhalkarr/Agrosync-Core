f85640af04e50e03145724ae893160dc4494a8de
Muhalkarr <muhammadabdulkholikarrasyid@gmail.com>
2026-06-18 13:04:48 +0700

    Vercel Preparation

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // 3. INSTRUKSI MUTLAK CPANEL: Bekukan aplikasi menjadi HTML Statis
+  output: 'export',
+
   // 1. Matikan optimasi gambar bawaan Next.js. cPanel tidak mendukung pemrosesan gambar dinamis.
   images: {
     unoptimized: true,
